5b343b5f3d0e0d4d4270f9fd1e7f5cae9582d04f
GarretCarmouche <garret307@yahoo.com>
2020-09-29 14:20:01 -0500

    Add documentation. Remove title from output. Minor Changes.

--- htmlconversion/HTMLConversion.java
+++ htmlconversion/HTMLConversion.java
@@ -1,12 +1,26 @@
-
+/*
+    Project 1
+    Garret Carmouche
+    CS2365-001 Fall 2020
+*/
 package htmlconversion;
 
 import java.io.*;
 import java.util.*;
 public class HTMLConversion {
 
+    /*
+        Prompt the user for the input HTML file path.
+        Read the file into a single string.
+
+        Input:
+        Output:
+        String - HTML content
+    */
    public static String readFile() throws FileNotFoundException {
-       File file = new File("TestFile.html");
+       System.out.println("Enter input HTML path");
+       Scanner pathScanner = new Scanner(System.in);
+       File file = new File(pathScanner.nextLine());
        String s = "";
        
        Scanner inputScanner = new Scanner(file);
@@ -17,27 +31,71 @@ public class HTMLConversion {
        return s;
    }
    
+   /*
+        Write a string into the output file
+        Input:
+        String s - String to be written to the file
+        PrintWriter outputWriter- PrintWriter s will be printed to
+        Output:
+   */
     public static void writeText(String s, PrintWriter outputWriter) throws FileNotFoundException{
-        String newS = s;
-        for(int i = 0; i < s.length()/80; i++){
-            //newS = newS + s.substring(i*80,(i+1)*80) + "\n";
+        for(int i = s.length(); i > 80; i -= 80){
+            int end;
+            if(s.length() <= 80){
+                end = s.length();
+            }else{
+                end = 80;
+            }
+            outputWriter.println(s.substring(0,end));
+            s = s.substring(end, s.length());
         }
-        //newS = newS + "\n";
-        outputWriter.println(newS);
+        outputWriter.println(s);
+        
     }
+    
+    /*
+        Write a string into the output file
+        Input:
+        String s - String to be written to the file
+        PrintWriter outputWriter- PrintWriter s will be printed to
+        boolean centered - Defines whether or not the text will be printed centered on the line. If not, left justified.
+        Output:
+   */
     public static void writeText(String s, PrintWriter outputWriter, boolean centered) throws FileNotFoundException{
         if(!centered){
             writeText(s, outputWriter);
             return;
         }
         
-        for(int i = 0; i < (80-s.length())/2; i++){
+        while(s.length() < 79){
             s = " " + s + " ";
         }
         outputWriter.println(s);
-    }  
+    }
     
-   
+    /*
+        Write a string into the output file
+        Input:
+        String s - String to be written to the file
+        PrintWriter outputWriter- PrintWriter s will be printed to
+        boolean centered - Defines whether or not the text will be printed centered on the line. If not, left justified.
+        boolean skipNextLine - Defines whether the line after s will be skipped.
+        Output:
+   */
+    public static void writeText(String s, PrintWriter outputWriter, boolean centered, boolean skipNextLine) throws FileNotFoundException{
+        writeText(s, outputWriter, centered);
+        if(skipNextLine){
+            outputWriter.println();
+        }
+    }
+    
+    /*
+        Recursively iterates through every open tag in the file to determine its behavior and whether it should be output to the txt file
+        Input:
+        String s - String containing HTML data
+        PrintWriter outputWriter - outputWriter s will be written to
+        Output:
+    */
    public static void nextTag(String s, PrintWriter outputWriter) throws FileNotFoundException{
        if(s.length() <= 0){
            return;
@@ -49,6 +107,7 @@ public class HTMLConversion {
         String tag = s.substring(openTag, endOpenTag+1);
         String text;
         int closeTag;
+        boolean skipNextLine = false;
         switch(tag){
             case "<h1>":
             case "<h2>":
@@ -56,7 +115,6 @@ public class HTMLConversion {
             case "<h4>":
             case "<h5>":
             case "<h6>":
-            case "<title>":
                 outputWriter.println();
                 if(s.substring(1).contains("<")){
                     closeTag = s.substring(1).indexOf("<");
@@ -70,7 +128,7 @@ public class HTMLConversion {
                 break;
                 
             case "<p>":
-                outputWriter.println();
+                skipNextLine = true;
             case "<td>":
             case "<th>":
             case "<li>":
@@ -82,7 +140,7 @@ public class HTMLConversion {
                
                 text = s.substring(endOpenTag+1,closeTag+1);
                 if(text.compareTo("") != 0)
-                    writeText(text, outputWriter);
+                    writeText(text, outputWriter, false, skipNextLine);
                 break;
         }
         if(!s.contains("<"))
@@ -96,6 +154,9 @@ public class HTMLConversion {
         nextTag(s, outputWriter);
    }
    
+   /*
+        Create output file, PrintWriter, invokes readFile and nextTag. Closes outputWriter.
+   */
     public static void main(String[] args) throws FileNotFoundException {
         File outputFile = new File("Output.txt");
         PrintWriter outputWriter = new PrintWriter(outputFile);
@@ -105,5 +166,4 @@ public class HTMLConversion {
         
         outputWriter.close();
     }
-    
 }
